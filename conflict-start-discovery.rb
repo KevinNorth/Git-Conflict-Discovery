e093f88e8575f6b4a41430180d5b415b879e2c6a
Kevin North <knorth@huskers.unl.edu>
2016-02-17 14:23:24 -0600

    Added logic for keeping track of branches

--- conflict-start-discovery.rb
+++ conflict-start-discovery.rb
@@ -1,8 +1,58 @@
 #!/usr/bin/env ruby
 
+class Branch
+  attr_reader :current_head
+
+  def initialize(commit)
+    @current_head = commit
+  end
+
+  def update_head(commit)
+    @current_head = commit
+  end
+end
+
+class Branches
+  def initialize
+    @branches = []
+  end
+
+  def advance(commit, parents)
+    matching_branches = []
+    for branch in @branches
+      if parents.include? (branch.current_head)
+        matching_branches.push branch
+      end
+    end
+
+    if matching_branches.count == 0
+      @branches.push Branch.new(commit)
+    elsif matching_branches.count == 1
+      matching_branches[0].update_head(commit)
+    else
+      for matching_branch in matching_branches
+        @branches.delete_if {|b| b.current_head.eql? (matching_branch.current_head)}
+      end
+      @branches.push Branch.new(commit)
+    end
+  end
+
+  def get_current_heads
+    return @branches.map {|b| b.current_head}
+  end
+
+  def get_current_heads_except(commit)
+    return self.get_current_heads().delete(commit)
+  end
+end
+
 working_dir = ARGV[0]
-Dir.chdir working_dir
+
+if working_dir != nil
+  Dir.chdir working_dir
+end
 
 graph = `git log --format="%H" --graph --no-color --author-date-order`.split("\n")
 
 puts graph.reverse
+
